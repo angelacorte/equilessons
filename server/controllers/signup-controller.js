6bfe7af90c64cf804adc24d0ccf96ab63d3c50ff
Angela <engy0798@hotmail.com>
2021-09-29 23:53:26 +0200

    now should update user's infos when sign up if he was temporary

--- server/controllers/signup-controller.js
+++ server/controllers/signup-controller.js
@@ -4,24 +4,36 @@ const User = db.user; //password already with salt
 const Club = require('./club-controller')
 
 exports.signup = function(req, res) {
-  //console.log(req.body);
   User.findOne({
     $or:[{
+      phoneNumber:req.body.phoneNumber
+    }] /*,{
       username: req.body.username
     },{
       email:req.body.email
-    }]
-  }).then(tmpUser => {
-    if (tmpUser)  {
+    }*/
+  }).then(u => {
+    if (u && u.email !== undefined)  {
       res.status(409).send({"description": "email or username already in use"})
-    }else{
+    }else{ //if user does not exists or exists without email (means that it's a temporary user, so it is possible to overwrite his infos)
       let newUser = new User(req.body);
-      newUser.save(function(err, user) {
-        if (err){
-          res.send(err);
-        }
-        res.status(200).json(user);
-      });
+      if(newUser.email !== undefined){
+        User.updateOne({phoneNumber: newUser.phoneNumber}).then(result=>{
+          if(result.ok !== 1){
+            return res.status(500).send({message: "an error occurred"});
+          }
+          return res.send(result);
+        }).catch(err=> {
+          console.log("Error: ", err.message);
+        });
+      }else{
+        newUser.save(function(err, user) {
+          if (err){
+            res.send(err);
+          }
+          res.status(200).json(user);
+        });
+      }
     }
   })
 };
