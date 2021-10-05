07d3fcc46d34d65267cccb3208418a091c7594f8
Angela <angela.cortecchia@hotmail.com>
2021-10-05 17:36:44 +0200

    create dialog view for showing users info in user management (clubs feature)

---
+++ src/app/dialog-view/dialog-view.component.ts
@@ -0,0 +1,49 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {UserService} from "../_services/user.service";
+import {map} from "rxjs/operators";
+
+export interface DialogData{
+  name: string,
+  surname: string,
+  phoneNumber: number,
+  email: string,
+  birthday: dateFns,
+  taxcode: string,
+  city: string,
+  address: string,
+  nrFise: any,
+  horse: any,
+  roles: any,
+  _id: any
+}
+
+@Component({
+  selector: 'app-dialog-view',
+  templateUrl: './dialog-view.component.html',
+  styleUrls: ['./dialog-view.component.css']
+})
+export class DialogViewComponent implements OnInit{
+
+  // @ts-ignore
+  user: DialogData;
+  horses: any;
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogViewComponent>, private userService: UserService,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onClose(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit(): void {
+    this.user = this.data;
+    if(this.data.horse.length > 0) this.horses = this.data.horse;
+
+  }
+
+  horseInfo(horseId: any) {
+    console.log("TO IMPLEMENT");
+  }
+}
