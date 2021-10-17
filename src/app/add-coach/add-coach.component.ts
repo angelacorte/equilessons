1a3b11ad15cb8fec85ca0ea26f9e2d99c2c7e9e5
Angela <angela.cortecchia@hotmail.com>
2021-10-17 22:52:21 +0200

    refactored and added paginator

--- src/app/add-coach/add-coach.component.ts
+++ src/app/add-coach/add-coach.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatTable} from "@angular/material/table";
+import {MatTable, MatTableDataSource} from "@angular/material/table";
 import {HttpClient} from "@angular/common/http";
 import {LessonService} from "../_services/lesson.service";
 import {TokenStorageService} from "../_services/token-storage.service";
-import {ArenaService} from "../_services/arena.service";
 import {ClubService} from "../_services/club.service";
-import {HorseService} from "../_services/horse.service";
-import {map} from "rxjs/operators";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-add-coach',
@@ -15,16 +15,21 @@ import {map} from "rxjs/operators";
 })
 export class AddCoachComponent implements OnInit {
 
-  // @ts-ignore
-  @ViewChild(MatTable) table: MatTable<any>;
+  @ViewChild(MatTable, {static:false}) table!: MatTable<any>;
+  @ViewChild(MatSort, {static:false})
+  set sort(value: MatSort) {
+    if (this.dataSource){
+      this.dataSource.sort = value;
+    }
+  }
+  @ViewChild(MatPaginator, {static:false}) paginator!: MatPaginator;
 
   form:any = {
     coachId: ''
   }
 
-  isSuccessful = false;
+  dataSource:any;
   errorMessage = '';
-  submitted = false;
   isLoggedIn = false;
   infos: any;
   displayedColumns = ['checkbox', 'istruttore'];
@@ -33,42 +38,57 @@ export class AddCoachComponent implements OnInit {
   users = [];
   toUpdate: {coachId: any}[]= [];
 
-  constructor(private http: HttpClient, private lessonService: LessonService, private tokenStorage: TokenStorageService, private clubService: ClubService) { }
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar, private lessonService: LessonService, private tokenStorage: TokenStorageService, private clubService: ClubService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.isLoggedIn = !!this.tokenStorage.getToken();
 
-    if(this.isLoggedIn && this.tokenStorage.isClub()) {
+    if (this.isLoggedIn && this.tokenStorage.isClub()) {
       this.infos = this.tokenStorage.getInfos(this.tokenStorage.isClub());
-      this.fetchData();
-    }else{
+
+      await this.getCoaches(this.infos._id).then((response)=>{
+        this.coaches = response[0]['clubCoaches'];
+      });
+
+      this.users = await this.getAthletes(this.infos._id);
+
+      this.setDataSource(this.coaches);
+    } else {
       window.location.assign('/notAllowed');
     }
   }
 
-  onSubmit() {
-    this.coaches.forEach((value)=>{
+  async onSubmit() {
+    this.coaches.forEach((value) => {
       this.toUpdate.push(value._id);
     })
-    this.clubService.addCoach(this.toUpdate, this.infos._id).subscribe(response=>{
-      this.submitted = true;
-      this.isSuccessful = true;
-      window.location.reload();
-    }, err => {
-      this.errorMessage = err.error.message;
-      console.log(err);
-    })
 
+    await this.addCoach(this.toUpdate, this.infos._id).then((response:any)=>{
+      if(response.ok === 1){
+        let snackBarRef = this._snackBar.open("Operazione effettuata con successo", "Aggiorna pagina", {
+          duration: 3000
+        });
+        snackBarRef.afterDismissed().subscribe(() => {
+          window.location.reload();
+        })
+      }else{
+        console.log(response);
+        let snackBarRef = this._snackBar.open("Errore nell'aggiornamento degli istruttori", "Aggiorna pagina", {
+          duration: 3000
+        });
+        snackBarRef.afterDismissed().subscribe(() => {
+          window.location.reload();
+        })
+      }
+    });
   }
 
   isUserInList(id: any):boolean {
-    // @ts-ignore
-    return this.coaches.some(obj=>obj["_id"] === id);
+    return this.coaches.some((obj)=>obj["_id"] === id);
   }
 
   addCoachToList(coachId: any) {
     this.users.forEach((value => {
-      // @ts-ignore
       if(value['_id'] === coachId && !this.coaches.some(obj=>obj['_id'] === coachId)){
         let coach = {
           _id: value['_id'],
@@ -77,50 +97,40 @@ export class AddCoachComponent implements OnInit {
         };
         this.coaches.push(coach);
         this.form.coachId = '';
+        this.setDataSource(this.coaches);
         this.table.renderRows();
       }
     }))
   }
 
-  private fetchData() {
-    this.clubService.getClubCoaches(this.infos._id).pipe(map(responseData =>{
-      const dataArray = [];
-      for ( const key in responseData){
-        if(responseData.hasOwnProperty(key)){
-          // @ts-ignore
-          dataArray.push({...responseData[key]})
-        }
-      }
-      return dataArray;
-    })).subscribe(response=>{
-      this.coaches = response[0].clubCoaches;
-    });
+  private async getCoaches(clubId:any): Promise<any>{
+    return this.clubService.getClubCoaches(clubId).toPromise();
+  }
 
-    this.clubService.getClubAthletes(this.infos._id).pipe(map(responseData =>{
-      const dataArray = [];
-      for ( const key in responseData){
-        if(responseData.hasOwnProperty(key)){
-          // @ts-ignore
-          dataArray.push({...responseData[key]})
-        }
-      }
-      return dataArray;
-    })).subscribe(response=>{
-      // @ts-ignore
-      this.users = response;
-      console.log("users", this.users);
-    });
+  private async getAthletes(clubId:any):Promise<any>{
+    return this.clubService.getClubAthletes(clubId).toPromise();
+  }
+
+  private async addCoach(update:any, clubId:any):Promise<any>{
+    return this.clubService.addCoach(update, clubId).toPromise()
   }
 
   isCoachUnchecked(e: any, coachId:any) {
     if(!e.target.checked){
-      // @ts-ignore
       this.coaches.forEach((item,index)=>{
         if(this.coaches[index] === coachId){
           this.coaches.splice(index, 1);
+          this.setDataSource(this.coaches);
           this.table.renderRows();
         }
       });
     }
+  }
+
+  private setDataSource(data:any){
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.data = data;
+    this.dataSource.sort;
+    this.dataSource.paginator = this.paginator;
   }
 }
