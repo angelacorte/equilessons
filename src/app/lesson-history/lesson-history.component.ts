b629b8056a673789635d09a63153aa16e91f3df7
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-05-18 23:47:32 +0200

    create lesson history component

---
+++ src/app/lesson-history/lesson-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-history',
+  templateUrl: './lesson-history.component.html',
+  styleUrls: ['./lesson-history.component.css']
+})
+export class LessonHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
