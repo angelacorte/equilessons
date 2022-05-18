d28f54dbdf5a5988ef1aa69f221225e2e85d31f4
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-05-18 23:47:22 +0200

    create personal infos component

---
+++ src/app/personal-infos/personal-infos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-infos',
+  templateUrl: './personal-infos.component.html',
+  styleUrls: ['./personal-infos.component.css']
+})
+export class PersonalInfosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
