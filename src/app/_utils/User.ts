9177d5eda24342cbe269b93d1c4ed2a548cb9e36
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-05-27 16:25:23 +0200

    create user interface

---
+++ src/app/_utils/User.ts
@@ -0,0 +1,14 @@
+export interface UserInfos{
+  name: string,
+  surname: string,
+  phoneNumber: number,
+  email: string,
+  birthday: dateFns, //todo change
+  taxcode: string,
+  city: string,
+  address: string,
+  nrFise: string,
+  horse: any,
+  roles: any,
+  _id: string
+}
