03591a9c3d7da133b15381c96ddd42bbc675258b
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-05-27 16:24:15 +0200

    add horse infos

--- src/app/dialog-horse-view/dialog-horse-view.component.ts
+++ src/app/dialog-horse-view/dialog-horse-view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {HorseInfos} from "../_utils/Horse";
+import {UserService} from "../_services/user.service";
 
 @Component({
   selector: 'app-dialog-horse-view',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogHorseViewComponent implements OnInit {
 
-  constructor() { }
+  horse!:HorseInfos;
+  riders: {_id: string, name: string, surname: string}[] = [];
+
+  constructor(private userService: UserService,
+              public dialogRef: MatDialogRef<DialogHorseViewComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: HorseInfos) { }
 
   ngOnInit(): void {
+    this.horse = this.data;
+    if(this.horse.riders.length > 0) {
+      this.horse.riders.forEach((riderId) => {
+        this.userService.getUserById(riderId).toPromise().then((res) => {
+          this.riders.push(res);
+        })
+      })
+    }
   }
 
+  onClose() {
+    this.dialogRef.close();
+  }
+
+  userInfo(_id: any) {
+    console.log("TO IMPLEMENT");
+  }
 }
