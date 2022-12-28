b648f85b55c4fde231000ca7ef52c492d86d07f2
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-12-28 16:13:58 +0100

    created notification schema

---
+++ server/models/notification-model.js
@@ -0,0 +1,14 @@
+const mongoose = require(`mongoose`)
+const Schema = mongoose.Schema
+
+let NotificationSchema = new Schema({
+    senderId: {type:Schema.Types.ObjectId, ref:"User", required: true},
+    recipientId: {type:Schema.Types.ObjectId, ref:"User", required: true},
+    lessonId: {type:Schema.Types.ObjectId, ref:"Lesson"},
+    lessonDate: {type: Date},
+    notificationType: {type: String, required: true},
+    notificationDate: {type: Date, required: true},
+    message: {type: String}
+})
+
+module.exports = mongoose.model("Notification", NotificationSchema, "notifications")
