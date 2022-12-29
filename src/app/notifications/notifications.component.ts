d0b964c2127fab6c442c6c1f376f97b396e7ad23
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-12-29 11:32:23 +0100

    added logic to retrieve notifications

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -8,6 +8,7 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 import {HorseService} from "../_services/horse.service";
 import {TokenStorageService} from "../_services/token-storage.service";
 import {NotificationMessage, NotificationType} from "../_utils/Notification";
+import { NotificationService } from '../_services/notification.service';
 
 @Component({
   selector: 'app-notifications',
@@ -26,22 +27,22 @@ export class NotificationsComponent implements OnInit {
   @ViewChild(MatPaginator, {static:false}) paginator!: MatPaginator;
 
   notifications:NotificationMessage[] = [];
-  displayedColumns = ['checkbox','sender', 'message', 'date']; //todo add read
+  displayedColumns = ['checkbox','sender', 'object', 'date', 'message'];
   dataSource = new MatTableDataSource(this.notifications);
   isClub: boolean = false;
   isLoggedIn: boolean = false;
-  constructor(private userService:UserService, public dialog: MatDialog, private _snackBar: MatSnackBar, private horseService: HorseService, private tokenStorage: TokenStorageService) { }
+  constructor(private notificationService: NotificationService, private userService:UserService, public dialog: MatDialog, private _snackBar: MatSnackBar, private horseService: HorseService, private tokenStorage: TokenStorageService) { }
 
   async ngOnInit(): Promise<void> {
     this.isClub = this.tokenStorage.isClub();
     this.isLoggedIn = !!this.tokenStorage.getToken();
+    let userId = this.tokenStorage.getInfos(this.isClub)._id
 
-    if(this.isLoggedIn && this.isClub){ //todo can send notifications
-      let not1: NotificationMessage = {sender: "miao", message: NotificationType.ADD, lessonID: "24199d", date: Date.now().toString(), lessonDate: "12/10/2022"}
-      let not2: NotificationMessage = {sender: "miao", message: NotificationType.MODIFY, lessonID: "24199d", date: Date.now().toString(), lessonDate: "12/10/2022"}
-      this.notifications.push(not1, not2)
-      console.log(this.notifications);
-      this.setDataSource(this.notifications);
+    if(this.isLoggedIn){ 
+      //call the service to retrieve user's notifications
+      this.notifications = await this.notificationService.getNotifications(userId)
+      console.log(JSON.stringify(this.notifications))
+      this.setDataSource(this.notifications)
     }
   }
 
