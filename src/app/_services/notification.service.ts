8f383a8cccab61d7d6afefcbeac3163517ffc376
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-12-29 11:31:18 +0100

    get all notifications of a certain user

--- src/app/_services/notification.service.ts
+++ src/app/_services/notification.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { NotificationMessage } from '../_utils/Notification';
+import { BASE_URL } from '../_utils/Global';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getNotifications() {
-
+  getNotifications(userId: string): Promise<any> {
+    return this.http.get(BASE_URL + '/notification/' + userId).toPromise()
   }
 }
