4b28445074119443b1ad2f7d4e9a7aa9ad2b670b
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-12-30 15:02:48 +0100

    create util file with enums

---
+++ src/app/_utils/Utils.ts
@@ -0,0 +1,11 @@
+export enum SnackBarActions {
+  REFRESH,
+  RETRY,
+  RELOAD
+}
+
+export enum SnackBarMessages{
+  OK = 'Azione compiuta con successo.',
+  RETRY = 'È stato riscontrato un problema, riprova.'
+
+}
