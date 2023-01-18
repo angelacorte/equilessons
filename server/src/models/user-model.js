83af2727b159449243e0e50c655838d48b23159e
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-01-18 15:30:52 +0100

    solved problem on login

--- server/src/models/user-model.js
+++ server/src/models/user-model.js
@@ -48,7 +48,8 @@ UserSchema.pre("save", function(next) {
   });
 });
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+UserSchema.methods.comparePassword = async function (candidatePassword, cb) {
+  let ret = await bcrypt.compare(candidatePassword, this.password)
   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch);
