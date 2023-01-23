5d40d4f4bf42c1d5ab4efb7c72de77d4e91351ee
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-01-23 13:09:52 +0100

    solved bug that wouldn't send club notification

--- server/src/models/notification-model.js
+++ server/src/models/notification-model.js
@@ -2,13 +2,14 @@ const mongoose = require(`mongoose`)
 const Schema = mongoose.Schema
 
 let NotificationSchema = new Schema({
-    senderId: {type:Schema.Types.ObjectId, ref:"User", required: true},
-    recipientId: [{type:Schema.Types.ObjectId, ref:"User", required: true}],
+    senderId: {type:Schema.Types.ObjectId, refPath: 'model_type', required: true},
+    recipientId: [{type:Schema.Types.ObjectId, refPath: 'model_type', required: true}],
     lessonId: {type:Schema.Types.ObjectId, ref:"Lesson"},
     lessonDate: {type: Date},
     notificationType: {type: String, required: true},
     notificationDate: {type: Date, required: true},
-    message: {type: String}
+    message: {type: String},
+    model_type: {type:String, enum:['User', 'Club']}
 })
 
 module.exports = mongoose.model("Notification", NotificationSchema, "notifications")
