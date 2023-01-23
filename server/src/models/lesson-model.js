a1213175b8a900c0f1ab3ded5ea28315421dc071
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-01-23 13:11:30 +0100

    refactor

--- server/src/models/lesson-model.js
+++ server/src/models/lesson-model.js
@@ -8,15 +8,13 @@ let  LessonSchema = new Schema({
   beginDate: {type: Date, required:true},
   endDate: {type: Date, required:true},
   arenaId: {type: Schema.Types.ObjectId, ref:"Arena", required:true},
-  coachId: {type:Schema.Types.ObjectId, ref:"User", required: true}, //refers to user coach
+  coachId: {type:Schema.Types.ObjectId, ref:"User", required: true},
   pairs: [{
     riderId: {type:Schema.Types.ObjectId, ref:"User"},
     horseId: {type:Schema.Types.ObjectId, ref:"Horse"}
   }],
-  //maxComponents: {type: Number, required: true},
-  clubId: {type:Schema.Types.ObjectId, ref:"Club", required: true}, //object/reference of 'club model'
+  clubId: {type:Schema.Types.ObjectId, ref:"Club", required: true},
   notes: {type: String, default:"Non ci sono note dell'istruttore."}
-  //color: {type: String}
 });
 
 module.exports = mongoose.model("Lesson", LessonSchema, "lessons");
